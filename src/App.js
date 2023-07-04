4b8c1c40e035d6378d50c4bbd42471501ac119a2
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-04 14:51:44 +0300

    Users list with filter, btn for add in invited list and  send some text  to invited people

--- src/App.js
+++ src/App.js
@@ -6,10 +6,55 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [success, setSuccess] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+  const [invetes, setInvetes] = React.useState([]);
+
+
+  //відправка запиту при першому рендері
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users') //отримуємо користувачів
+      .then(res => res.json()) //преобразуємо в json
+      .then(json => setUsers(json.data)) //записуємо користувачів
+      .catch(err => console.warn(err)) //виводимо помилку
+      .finally(() => setIsLoading(false)); //відображаємо юзерів замість скелетонів
+  }, [])
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
+  const onClickInvite = (id) => {
+    if (invetes.includes(id)) {
+      setInvetes(prev => prev.filter(_id => _id !== id));
+    } else {
+      setInvetes(prev => [...prev, id]);
+    }
+  }
+
+  const onClickSendInvite = () => {
+    setSuccess(true);
+  }
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {
+        success ? (
+          <Success count={invetes.length} />
+        ) : (
+          <Users
+            onChangeSearchValue={onChangeSearchValue}
+            searchValue={searchValue}
+            items={users}
+            isLoading={isLoading}
+            onClickInvite={onClickInvite}
+            invetes={invetes}
+            onClickSendInvite={onClickSendInvite}
+          />
+        )
+      }
     </div>
   );
 }
