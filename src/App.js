918e7fddaa3a78f98f4fb3a9baa0fbb3f80aecb5
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-04 13:04:23 +0300

    Game component (show questions and add function to each answer to increase the step and calculate the correct answer), Result component - number of correct answers and restart game btn(nullification step and correct answers), App component - step != qusetions.length => show needed component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,88 @@
 import './index.scss';
+import React from 'react';
 
+//список запитань який містить заголовок, варіанти відповідей і індекс правильної відповіді
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: 'React - це ... ?',
+    variants: ['бібліотека', 'фреймворк', 'додаток'],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: 'Компонент - це ... ',
+    variants: ['додаток', 'частина додатку або сторінки', 'те, що я не знаю що це таке'],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: 'Що таке JSX?',
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      'Це простий HTML',
+      'Це функція',
+      'Це той же HTML, але з можливістю виконувати JS-код',
     ],
     correct: 2,
   },
 ];
 
-function Result() {
+//компонента результат - функція для початку нової гри та кількість парвильних відповідей
+function Result({ restartGame, correct}) {
   return (
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <h2>Ви відгадали {correct} відповіді з {questions.length}</h2>
+      <button onClick={restartGame}>Спробувати знову</button>
     </div>
   );
 }
 
-function Game() {
+//
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
   return (
     <>
+    {/* прогрес бар - на якому ти кроці */}
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
+      {/* рендер відповідей списком */}
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, index) => (
+          <li onClick={()=> onClickVariant(index)} key={text}>{text}</li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = React.useState(0);
+  const [correct, setCorrect] = React.useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+
+    if(index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  }
+
+  const restartGame = () => {
+    setStep(0);
+    setCorrect(0);
+  }
+
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant}/>
+      ) : (
+        <Result restartGame={restartGame} correct={correct} />
+      )
+
+      }
     </div>
   );
 }
