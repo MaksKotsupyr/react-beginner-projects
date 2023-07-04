02f112c36debd9abab60c2650bcf5ab742ec5b36
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-04 15:50:48 +0300

    Currency converter (re-calculate when change currency and its amount)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,60 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  // const [rates, setRates] = React.useState({});
+  const [fromCurrency, setFromCurrency] = React.useState('UAH');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
+
+  const ratesRef = React.useRef({});
+
+  React.useEffect(() => {
+    fetch('https://api.exchangerate.host/latest')
+      .then((res) => res.json())
+      .then((data) => {
+        ratesRef.current = data.rates;
+        onChangeToPrice(1);
+      }).catch((err) => {
+        console.warn(err);
+      })
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setFromPrice(value);
+    setToPrice(result.toFixed(3));
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(3));
+    setToPrice(value);
+  }
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency])
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency])
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block 
+        value={fromPrice} 
+        currency={fromCurrency} 
+        onChangeCurrency={setFromCurrency} 
+        onChangeValue={onChangeFromPrice} 
+      />
+      <Block 
+        value={toPrice} 
+        currency={toCurrency} 
+        onChangeCurrency={setToCurrency} 
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
