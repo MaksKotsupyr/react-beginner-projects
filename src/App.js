64d98173ceb098c4a7bac9cc5b30dd29688b7745
KOTSUPYR MAKSYM <maksym.kotsupyr@gmail.com>
2023-07-04 11:10:07 +0300

    React counter using useState and 2 simple functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './index.scss';
+import React from 'react';
 
 function App() {
+  //створюємо стан змінної і функцію для зміни значення
+  // 0 - початкове значення стану і цей хук сповіщає Реакт про зміну і заставляє перемалювати її
+  const [count, setCount] = React.useState(0); 
+
+  //створюємо функцію для зміни значення(+)
+  const onClickPlus = () => {
+    setCount(count + 1);
+  }
+
+  //створюємо функцію для зміни значення(-)
+  const onClickMinus = () => {
+    setCount(count - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );
 }
 
 export default App;
+
